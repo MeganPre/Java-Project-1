eecf84ab3409e94261aeaa12b8ae5d01fca18fb5
Megan Pretorius <10207@ctucareer.co.za>
2022-11-04 10:07:28 +0200

    Java Project 1

---
+++ src/gui_anime/GUI_Anime.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui_anime;
+
+/**
+ *
+ * @author megan
+ */
+public class GUI_Anime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.setVisible(true);
+    }
+    
+}
